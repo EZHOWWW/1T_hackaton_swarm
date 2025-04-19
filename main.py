4c0462c1e4532cccec1e5b6541ed7e486fb0414e
ezhowww <ezhoff.dmitriy@gmail.com>
2025-04-19 17:25:38 +0300

    add main solution

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import asyncio
-import json
-
 from config import LOG_LEVEL
 from services.logger import set_logger_config
 
 
 async def start_websocket():
-    from algorithm.fly import connection
-    from algorithm.PID import concat_engine, concat_engines
     from solution.simulation import Simulation
     from solution.swarm import Swarm
 
     sim = Simulation()
     await sim.connect_to_server()
-    sim.get_drone_info(0)
 
+    # here is solution
     swarm = Swarm(sim)
-
     swarm.start()
 
     sim.close_connection()
