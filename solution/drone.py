3b00cbee455dff04d3aa1808cc583381e7a71aaf
ezhowww <ezhoff.dmitriy@gmail.com>
2025-04-19 17:39:34 +0300

    do find fireplace tast and start go to task

--- solution/drone.py
+++ solution/drone.py
@@ -1,6 +1,7 @@
 import asyncio
 from solution.simulation import Simulation, Fireplace
 from abc import ABC
+from solution.geometry import Vector
 
 
 class Task(ABC):
@@ -11,13 +12,17 @@ class FindFireplace(Task):
     pass
 
 
+class Sleep(Task):
+    pass
+
+
 class GoToTask(Task):
-    def __init__(self, pos):
+    def __init__(self, pos: Vector):
         self.pos = pos
 
 
 class GoToFireplace(GoToTask):
-    def __init__(self, fireplace_pos):
+    def __init__(self, fireplace_pos: Vector):
         super().__init__(fireplace_pos)
 
 
@@ -27,41 +32,86 @@ class GoToHome(GoToTask):
 
 
 class DroneExecutor:
-    def __init__(self, number: int, sim: Simulation):
-        self.number = number
-        self.sim = sim
-        self.cur_task = None
+    def __init__(self, drone):
+        self.drone = drone
+        self.engine_possition_ration = t = 0.25
+        z = 1 - (t**2 + (1 - t) ** 2)  # vector len = 1
+        self.engines_vector_effect = [
+            Vector(t, 1 - t, z),  # 1 fr
+            Vector(-t, 1 - t, z),  # 2 fl
+            -Vector(-t, 1 - t, z),  # 3 br
+            -Vector(t, 1 - t, z),  # 4 bl
+            Vector(1 - t, t, z),  # 5 rf
+            Vector(1 - t, -t, z),  # 6 rb
+            -Vector(1 - t, -t, z),  # 7 lf
+            -Vector(1 - t, t, z),  # 8 lb
+        ]
+        self.desired_speed = Vector()
 
-    def set_motors(self, motors: tuple[float]):
-        self.sim.set_drone_motors(self.number, motors)
+    def engines_for_speed(self, desired_speed: Vector = Vector()) -> list[float]:
+        from numpy import clip
+
+        self.desired_speed = desired_speed
+
+        engines = [0] * 8
+        for i, v in enumerate(self.engines_vector_effect):
+            engines[i] = self.desired_speed.dot(v)
+        # if < 0 need up other engine
+        return clip(engines, 0, 1)
 
 
 class Drone:
-    def __init__(self, number: int, sim: Simulation, swarm):
+    def __init__(self, id: int, sim: Simulation, swarm):
         self.active = True
-        self.number = number
-        self.executor = DroneExecutor(self.number, sim)
+        self.id = id
+        self.executor = DroneExecutor(self)
         self.sim = sim
         self.swarm = swarm
         self.task = FindFireplace()
-        self.self_info = None
-
-    def start(self):
-        while self.active:
-            self.update()
+        # Main params
+        self.params = None
+        self.engines = None
+        self.need_drop = False
 
     def update(self):
-        self.self_info = self.sim.get_drone_info(self.number)
-        # match type(self.task):
-        #     case FindFireplace:
-        #         self.find_fireplace(self.swarm.fireplaces)
-        #     case GoToTask:
-        #         pass
+        """now self.params and self.engines is actual"""
+        print(self.params)
+        print(self.engines)
+        print()
 
-    def find_fireplace(self, fireplaces: list[list[Fireplace, int]]) -> int:
+        if isinstance(self.task, FindFireplace):
+            pos = self.find_fireplace(self.swarm.fireplaces)
+            if pos is not None:
+                self.task = GoToFireplace(pos)
+            else:
+                self.task = Sleep()
+        if isinstance(self.task, GoToFireplace):
+            self.go_to_fireplace(self.task)
+
+    def go_to_fireplace(self, fireplace_task: GoToFireplace):
+        direction = fireplace_task.pos - self.params.possition
+        direction = Vector(0, 0, 1)
+        print(direction)
+        self.engines = self.executor.engines_for_speed(direction)
+
+    def find_fireplace(self, fireplaces: list[list[Fireplace, int]]) -> Vector | None:
         """
         get list(Fireplace(pos, active), drone number which work on in)
         return number Fireplace which we choose
         """
-        # l =
-        pass
+        fireplace_id = 0
+        l = [
+            (self.params.possition - v[0].possition).length()
+            for i, v in enumerate(fireplaces)
+            if v[1] == -1
+        ]
+        if len(l) == 0:
+            return None
+        print(l)
+        fireplace_id = l.index(min(l))
+        fireplaces[fireplace_id][1] = self.id
+
+        print(
+            f"drone: {self.id} with pos: {self.params.possition} \nfind fireplace id: {fireplace_id} \nwith pos: {fireplaces[fireplace_id][0].possition}, \ndistance: {l[fireplace_id]}\n"
+        )
+        return fireplaces[fireplace_id][0].possition
