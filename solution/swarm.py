81b1e80c7a711aa8466925123b33aa875b826ebe
ezhowww <ezhoff.dmitriy@gmail.com>
2025-04-19 21:14:00 +0300

    base solution start

--- solution/swarm.py
+++ solution/swarm.py
@@ -1,5 +1,6 @@
 from solution.simulation import Simulation
 from solution.drone import Drone
+from solution.geometry import Vector
 import time
 
 
@@ -7,17 +8,21 @@ class Swarm:
     def __init__(self, sim: Simulation, unit_count: int = 5):
         self.sim = sim
         self.unit_count = unit_count
-        self.units = [Drone(i, self.sim, self) for i in range(self.unit_count)]
+        self.units = [Drone(self.sim, self) for i in range(self.unit_count)]
         self.fireplaces = None
 
     def start(self):
         self.fireplaces = [[i, -1] for i in self.sim.get_fireplaces_info()]
         print(self.fireplaces)
 
+        dt = 1
+        start = time.time()
         while True:
             self.update_drones_info()
+            dt = time.time() - start
+            start = time.time()
             for u in self.units:
-                u.update()
+                u.update(dt * 10)
             self.upload_drones_info()
             time.sleep(1.0)
 
@@ -28,6 +33,9 @@ class Swarm:
             v.engines = self.sim.last_engines[i]
 
     def calculate_home_squere(self):
+        pass
+
+    def get_home_pos(self, target: Vector) -> Vector:
         pass
 
     def upload_drones_info(self):
